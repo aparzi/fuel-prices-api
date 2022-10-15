f90088c6992d28eeaf33fa8cc509d5c49664cbb6
aparziale <angelo.parziale@democom.it>
2022-10-15 17:59:45 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+// importing the dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const axios = require('axios');
+
+const app = express();
+
+// adding Helmet to enhance your Rest API's security
+app.use(helmet());
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+
+// defining an endpoint to return all ads
+app.get('/', (req, res) => {
+    res.send({success: 'ok', message: 'Hello world...'});
+});
+
+app.post('/search/zone', async (req, res) => {
+    try {
+        const points = req.body.points;
+        if (!points) {
+            return res.status(400).json({success: false, message: 'points is mandatory'});
+        }
+
+        const response = await axios.post('https://carburanti.mise.gov.it/ospzApi/search/zone', {
+            "points": points,
+            "fuelType": null
+        });
+        console.log('response => ', response);
+        res.json(response.data);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+// starting the server
+app.listen(3100, () => {
+    console.log('listening on port 3100');
+});
